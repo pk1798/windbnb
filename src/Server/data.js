3989ad497795c294805d72167de0ab553d0f94cd
Praveen Kumar <pk1798.kumar@gmail.com>
2021-07-13 00:39:31 +0530

    added more hotels

--- src/Server/data.js
+++ src/Server/data.js
@@ -58,6 +58,311 @@ const data = [{
     rating: 4.64,
     host: 'regular',
     id:6
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: "Trichy",
+    type: "Entire apartment . 2 beds",
+    guest:4,
+    rating: 4.40,
+    host: 'super',
+    id:1
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Coimbatore',
+    type: "Private room",
+    guest:5,
+    rating: 4.25,
+    host: 'regular',
+    id:2
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: 'Chennai',
+    type: "Entire house",
+    guest:6,
+    rating: 4.96,
+    host: 'regular',
+    id:3
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Dindugal',
+    type: "Entire apartment . 2 beds",
+    guest:4,
+    rating: 4.85,
+    host: 'super',
+    id:4
+  
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: 'Trichy',
+    type: "Private room",
+    guest:2,
+    rating: 4.54,
+    host: 'regular',
+    id:5
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Chennai',
+    type: "Entire house",
+    guest:8,
+    rating: 4.64,
+    host: 'regular',
+    id:6
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: "Trichy",
+    type: "Entire apartment . 2 beds",
+    guest:4,
+    rating: 4.40,
+    host: 'super',
+    id:1
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Coimbatore',
+    type: "Private room",
+    guest:5,
+    rating: 4.25,
+    host: 'regular',
+    id:2
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: 'Chennai',
+    type: "Entire house",
+    guest:6,
+    rating: 4.96,
+    host: 'regular',
+    id:3
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Dindugal',
+    type: "Entire apartment . 2 beds",
+    guest:4,
+    rating: 4.85,
+    host: 'super',
+    id:4
+  
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: 'Trichy',
+    type: "Private room",
+    guest:2,
+    rating: 4.54,
+    host: 'regular',
+    id:5
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Chennai',
+    type: "Entire house",
+    guest:8,
+    rating: 4.64,
+    host: 'regular',
+    id:6
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: "Trichy",
+    type: "Entire apartment . 2 beds",
+    guest:4,
+    rating: 4.40,
+    host: 'super',
+    id:1
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Coimbatore',
+    type: "Private room",
+    guest:5,
+    rating: 4.25,
+    host: 'regular',
+    id:2
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: 'Chennai',
+    type: "Entire house",
+    guest:6,
+    rating: 4.96,
+    host: 'regular',
+    id:3
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Dindugal',
+    type: "Entire apartment . 2 beds",
+    guest:4,
+    rating: 4.85,
+    host: 'super',
+    id:4
+  
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: 'Trichy',
+    type: "Private room",
+    guest:2,
+    rating: 4.54,
+    host: 'regular',
+    id:5
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Chennai',
+    type: "Entire house",
+    guest:8,
+    rating: 4.64,
+    host: 'regular',
+    id:6
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: "Trichy",
+    type: "Entire apartment . 2 beds",
+    guest:4,
+    rating: 4.40,
+    host: 'super',
+    id:1
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Coimbatore',
+    type: "Private room",
+    guest:5,
+    rating: 4.25,
+    host: 'regular',
+    id:2
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: 'Chennai',
+    type: "Entire house",
+    guest:6,
+    rating: 4.96,
+    host: 'regular',
+    id:3
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Dindugal',
+    type: "Entire apartment . 2 beds",
+    guest:4,
+    rating: 4.85,
+    host: 'super',
+    id:4
+  
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: 'Trichy',
+    type: "Private room",
+    guest:2,
+    rating: 4.54,
+    host: 'regular',
+    id:5
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Chennai',
+    type: "Entire house",
+    guest:8,
+    rating: 4.64,
+    host: 'regular',
+    id:6
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: "Trichy",
+    type: "Entire apartment . 2 beds",
+    guest:4,
+    rating: 4.40,
+    host: 'super',
+    id:1
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Coimbatore',
+    type: "Private room",
+    guest:5,
+    rating: 4.25,
+    host: 'regular',
+    id:2
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: 'Chennai',
+    type: "Entire house",
+    guest:6,
+    rating: 4.96,
+    host: 'regular',
+    id:3
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Dindugal',
+    type: "Entire apartment . 2 beds",
+    guest:4,
+    rating: 4.85,
+    host: 'super',
+    id:4
+  
+},
+{
+    image: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80',
+    name: 'Stylist apartment in center of the city',
+    location: 'Trichy',
+    type: "Private room",
+    guest:2,
+    rating: 4.54,
+    host: 'regular',
+    id:5
+},
+{
+    image: 'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'Cozy, peaceful and private room with',
+    location: 'Chennai',
+    type: "Entire house",
+    guest:8,
+    rating: 4.64,
+    host: 'regular',
+    id:6
 }]
 
 export default data;
